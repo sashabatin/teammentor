380a9062b407459d6bd0753ae546d0eef1d07e80
sashabatin <130882806+sashabatin@users.noreply.github.com>
2025-07-10 23:06:53 +0400

    changes

--- teammentor.py
+++ teammentor.py
@@ -1,12 +1,14 @@
 import logging
-from flask import Flask, request, jsonify
+import os
+import requests
+import azure.functions as func
 from telegram import Bot
 
-app = Flask(__name__)
+app = func.FunctionApp()
 
-@app.route('/api/query', methods=['POST'])
-def handle_telegram_update():
-    telegram_data = request.get_json()
+@app.route(route="query", auth_level=func.AuthLevel.ANONYMOUS)
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    telegram_data = req.get_json()
     chat_id = telegram_data.get('message', {}).get('chat', {}).get('id')
     user_message = telegram_data.get('message', {}).get('text', '')
 
@@ -14,16 +16,17 @@ def handle_telegram_update():
         response_text = query_assistant(user_message)
         send_message_to_telegram(chat_id, response_text)
 
-    return '', 200
+    return func.HttpResponse(status_code=200)
 
 def query_assistant(message):
-    api_url = app.config['ASSISTANT_API_URL']
-    response = requests.post(api, json={'query': message}).json()
-    return response.get('response', "Sorry, I couldn't understand your request.")
+    api_url = os.environ['ASSISTANT_API_URL']
+    response = requests.post(api_url, json={'query': message})
+    return response.json().get('response', "Sorry, I couldn't understand your request.")
 
 def send_message_to_telegram(chat_id, message):
-    telegram_api_url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
-    response = requests.post(telegram_api_url, json={
+    telegram_token = os.environ['TELEGRAM_BOT_TOKEN']
+    telegram_api_url = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
+    requests.post(telegram_api_url, json={
         'chat_id': chat_id,
         'text': message
     })
